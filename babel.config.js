203ff5d71a997799b61654bb511a00fc00d1fce2
David <daichangya@163.com>
2025-11-09 21:16:25 +0800

    11

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-};
+export default class NotificationPlugin {
+  constructor() {
+    this.name = 'Notification Plugin';
+    this.version = '1.0.0';
+    this.description = '显示文章信息通知';
+    this.icon = '🔔';
+  }
+
+  async execute(context) {
+    const message = `文章标题: ${context.title}\n作者: ${context.author || '未知'}\n来源: ${context.url}`;
+    alert(message);
+  }
+}
